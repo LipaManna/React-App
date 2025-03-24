d07d7f846a91ce2998277466296a07b46be61845
Lipa <lipamanna5@gmail.com>
2025-03-25 00:12:25 +0530

    Add email validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { Component } from 'react'
-import { Provider } from './components/ui/provider'
-import { Route, Routes } from 'react-router-dom'
-import Login from './Pages/Login'
-import './assets/custom.css'
+import { Provider } from "./components/ui/provider";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import "./assets/custom.css";
+import ErrorBoundary from "./shared/ErrorBoundary";
 
 function App() {
   return (
     <Provider>
-     <Routes>
-        <Route path="/" element={<Login/>} />
-      </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ErrorBoundary
+              children={<Login />}
+              fallback={<></>}
+            ></ErrorBoundary>
+          }
+        />
+      </Routes>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Card,
@@ -8,8 +8,22 @@ import {
   Stack,
 } from "@chakra-ui/react";
 import { Field } from "../components/ui/field";
+import { emailValidator } from "../utils/EmailValidator";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+
+  const handleEmailBlur = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    setEmail(e.target.value);
+    !emailValidator(e.target.value) ? setError("Invalid Email!") : setError("");
+  };
+
   return (
     <Container px="2" className="mainContainer formContainer">
       <Card.Root
@@ -18,37 +32,43 @@ const Login = () => {
         backgroundColor={"blackAlpha.400"}
         p="10"
       >
-        
-          <Stack gap="4" w="full">
-            <Heading as={"h2"} textAlign={"center"} size={"3xl"}>
-              Welcome Back!
-            </Heading>
-            <form action="" className="formWrap">
-              <Field label="Email ID">
-                <Input placeholder="" type="email" />
-              </Field>
-              <Field label="Password" mt="5">
-                <Input placeholder="" type="password" />
-              </Field>
-              <Button
-                rounded="sm"
-                type="submit"
-                mt="5"
-                _hover={{
-                  borderColor: "whiteAlpha.700",
-                  bg: "transparent",
-                  color: "whiteAlpha.700",
-                }}
-                mx="auto"
-                display="block"
-                size={"lg"}
-                w={"100%"}
-              >
-                Login
-              </Button>
-            </form>
-            <a href="#" color="whiteAlpha.700" className="forgotPass">Forgot password?</a>
-          </Stack>
+        <Stack gap="4" w="full">
+          <Heading as={"h2"} textAlign={"center"} size={"3xl"}>
+            Welcome Back!
+          </Heading>
+          <form action="" className="formWrap" onSubmit={handleSubmit}>
+            <Field label="Email ID">
+              <Input
+                placeholder=""
+                type="email"
+                onBlur={handleEmailBlur}
+              />
+            </Field>
+            {error && <p>{error}</p>}
+            <Field label="Password" mt="5">
+              <Input placeholder="" type="password" />
+            </Field>
+            <Button
+              rounded="sm"
+              type="submit"
+              mt="5"
+              _hover={{
+                borderColor: "whiteAlpha.700",
+                bg: "transparent",
+                color: "whiteAlpha.700",
+              }}
+              mx="auto"
+              display="block"
+              size={"lg"}
+              w={"100%"}
+            >
+              Login
+            </Button>
+          </form>
+          <a href="#" color="whiteAlpha.700" className="forgotPass">
+            Forgot password?
+          </a>
+        </Stack>
       </Card.Root>
     </Container>
   );
